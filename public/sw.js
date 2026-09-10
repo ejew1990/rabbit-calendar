7ad45bd7ef8149a6fcb4fa25c26893e30843f9b6
ejew1990 <xiaoyizhu1990@yahoo.com>
2026-09-10 14:15:44 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+// Service Worker for Bunny Family Calendar Notifications
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// 点击通知栏时唤醒网页
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
